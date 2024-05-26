c144fe421ca1f14fb32a5bb854a29d04e2989238
rzn0v <126226011+rzn0v@users.noreply.github.com>
2024-05-26 15:39:55 +0530

    Add files via upload

---
+++ US states quiz/main.py
@@ -0,0 +1,32 @@
+import turtle, pandas as pd
+
+screen = turtle.Screen()
+screen.title("US states game")
+image = "blank_states_img.gif"
+turtle.addshape(image)
+turtle.shape(image)
+data = pd.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_state = []
+missing_states = []
+while len(guessed_state)<50:
+    ans_state = screen.textinput(title=f"{len(guessed_state)}/50 states guessed", prompt="Type the state").title()
+    if ans_state == "Exit":
+        for states in all_states:
+            if states not in guessed_state:
+                missing_states.append(states)
+
+        new_data = pd.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if ans_state in all_states:
+        guessed_state.append(ans_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        coordinates = data[data.state == ans_state]
+        t.goto(int(coordinates.x), int(coordinates.y))
+        t.write(ans_state)
+
+
+screen.exitonclick()
